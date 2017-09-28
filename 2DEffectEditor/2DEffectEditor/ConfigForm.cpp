85d2be90183303b7a183de1fdc3c074360b6a097
Masashi <masashi77823@gmail.com>
2017-09-28 10:24:52 +0900

    Add UI

---
+++ 2DEffectEditor/2DEffectEditor/ConfigForm.cpp
@@ -0,0 +1,2 @@
+#include "ConfigForm.h"
+
---
+++ 2DEffectEditor/2DEffectEditor/ParameterForm1.h
@@ -0,0 +1,195 @@
+#pragma once
+
+namespace My2DEffectEditor {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// ParameterForm1 の概要
+	/// </summary>
+	public ref class ParameterForm1 : public System::Windows::Forms::Form
+	{
+	public:
+		ParameterForm1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ここにコンストラクター コードを追加します
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		~ParameterForm1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::Label^  label4;
+
+
+
+
+	private:
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->BackColor = System::Drawing::SystemColors::Control;
+			this->groupBox1->Controls->Add(this->comboBox1);
+			this->groupBox1->Controls->Add(this->label4);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->ImeMode = System::Windows::Forms::ImeMode::Alpha;
+			this->groupBox1->Location = System::Drawing::Point(27, 27);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(389, 246);
+			this->groupBox1->TabIndex = 5;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"発生";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FlatStyle = System::Windows::Forms::FlatStyle::System;
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->ImeMode = System::Windows::Forms::ImeMode::Off;
+			this->comboBox1->Location = System::Drawing::Point(186, 164);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(179, 26);
+			this->comboBox1->TabIndex = 2;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(32, 173);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(99, 18);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"発生パターン";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(301, 106);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(64, 18);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"フレーム";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(31, 40);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(62, 18);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"発生数";
+			// 
+			// textBox2
+			// 
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox2->ImeMode = System::Windows::Forms::ImeMode::Disable;
+			this->textBox2->Location = System::Drawing::Point(186, 102);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(101, 25);
+			this->textBox2->TabIndex = 1;
+			this->textBox2->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// textBox1
+			// 
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox1->ImeMode = System::Windows::Forms::ImeMode::Disable;
+			this->textBox1->Location = System::Drawing::Point(186, 38);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(101, 25);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(31, 104);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(100, 18);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"発生フレーム";
+			// 
+			// ParameterForm1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(10, 18);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(445, 285);
+			this->Controls->Add(this->groupBox1);
+			this->Name = L"ParameterForm1";
+			this->Text = L"ParameterForm1";
+			this->Load += gcnew System::EventHandler(this, &ParameterForm1::ParameterForm1_Load);
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+
+	private: System::Void ParameterForm1_Load(System::Object^  sender, System::EventArgs^  e) {
+		comboBox1->Items->Add(L"一方向");
+		comboBox1->Items->Add(L"拡散");
+		comboBox1->Items->Add(L"集約");
+		comboBox1->Items->Add(L"なんかすげーやつ");
+
+		comboBox1->DropDownStyle = ComboBoxStyle::DropDownList;
+
+		comboBox1->SelectedIndex = 0;
+	}
+};
+
+}
